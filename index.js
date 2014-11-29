d3e479348212360df7010e8dd035d84423c4151b
vork <itsallasham@gmail.com>
2014-11-29 10:50:09 -0600

    added blank req handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 
+handle[""] = requestHandlers.start;
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
