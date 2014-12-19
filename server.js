d4e18e1177d3207969847267779e206f76ba9e82
vork <itsallasham@gmail.com>
2014-12-18 19:38:12 -0600

    third

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var server = http.createServer(function onReqest(req, res) {
 
 });
 
-server.listen(8888, function() {
+server.listen(5000, function() {
 	util.log('Server Listening on port 8888');
 });
 }
