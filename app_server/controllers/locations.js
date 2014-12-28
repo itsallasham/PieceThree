8ee2efd8e0da3794d3a75af339b27893067ed816
vork <itsallasham@gmail.com>
2014-12-27 19:52:04 -0600

    added 'add review'

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -26,15 +26,47 @@ module.exports.home = function(req,res) {
 			facilities: ['Food', 'Premium wifi'],
 			distance: '250m'
 		}]
+
+		
 	});
 };
 
 /*get location info page*/
 module.exports.locationInfo = function(req,res) {
-	res.render('index', {title: 'Location Info'});
-};
+	
+
+
+	res.render('location', {
+		title: 'Location Info - Does it blow?',
+		pageHeader: {
+			title: 'Not THIS ONE',
+			strapline: 'Does it suck?'
+		},
+		sidebar: "We can help take it to your friends mom!",
+		locations: [{
+			name: 'Starcups',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			distance: '100m',
+			hours: [
+				'Monday - Friday: 7:00am - 7:00pm',
+				'Saturday: 8:00am - 5:00pm',
+				'Sunday: Closed'
+			]
+		}]
+	});
+
+};	
+
  
 /*get add review page*/
 module.exports.addReview = function(req, res) {
-	res.render('index', {title: 'Add Review'});
+	res.render('addReview', {
+		title: 'Add Review',
+		pageHeader: {
+			title: 'Add review',
+			strapline: 'Share your experience with the world'
+		}
+	});
 };
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,10 +1,6 @@
-/*get landing page*/
-exports.landing = function(req, res) {
-	res.render('locations-list', {title: 'P-IIV'});
-};
+
 
 /*get about us page*/
-
 module.exports.about = function(req, res) {
 	res.render('index', {title: 'About us'});
 };
