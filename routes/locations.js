ab3f8d1a168053feb9fa243e8b3f9fa2c9fa461c
vork <itsallasham@gmail.com>
2015-01-09 15:18:14 -0600

    added / to .get

--- routes/locations.js
+++ routes/locations.js
@@ -2,7 +2,7 @@ var ctrl = require('../app_server/controllers/locations');
 
 module.exports = function(app) {
 	app.get('', ctrl.home);
-	app.get('', ctrl.home);
+	app.get('/', ctrl.home);
 	app.get('/location/:locationid', ctrl.locationInfo);
 	app.get('/location/review/new', ctrl.addReview);
 }; 
