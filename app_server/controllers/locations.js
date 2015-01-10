da6467f1427016e59c3aa3410e69739d456218b8
vork <itsallasham@gmail.com>
2015-01-09 21:51:13 -0600

    fixed typo:

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -64,7 +64,7 @@ module.exports.home = function(req,res) {
 			maxDistance: 30
 		}
 	};
-	console.log(requestOption.url);
+	console.log(requestOptions.url);
 	request (
 		requestOptions,
 		function(err, response, body) {
